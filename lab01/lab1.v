359a4e1d2d9091ad5d89ae2ab118dbe50ed0fa2d
Tyler Hall <apple@Tylers-MacBook-Pro.local>
2015-02-02 23:32:09 -0500

    submitted lab 1

---
+++ lab01/lab1.v
@@ -0,0 +1,13 @@
+module lab1(OutF, InA, InB, InC);
+output OutF;
+input InA, InB, InC;
+
+wire notB; //wire for not B
+wire BC; //output of B * C;
+
+not(notB, InB);
+
+and(BC, InC, notB);
+or(OutF, BC, InA);
+
+endmodule
