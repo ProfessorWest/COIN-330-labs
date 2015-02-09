4f352c7f3584d797f4aae9725ef102a7a8d73320
unknown <pwest@WestP.csuniv.edu>
2015-02-09 05:01:48 -0500

    comments

--- lab01/trevor_yager_lab1_test.v
+++ lab01/trevor_yager_lab1_test.v
@@ -12,6 +12,12 @@ initial begin
 
   InA = 1'b1;
   InB = 1'b1;
+  /* The value below is not valid.
+   * 0'b1 says to create 0-length bit field with value 1.  (This makes no
+   * sense.)
+   * I believe you meant: 1'b0.
+   * Due to how Icarus Verilog works 0b'1 resolves to 0.
+   */
   InC = 0'b1;
   #1 $display("InA = %b, InB = %b, InC = %b, Out = %b", InA, InB, InC, Out);
 
