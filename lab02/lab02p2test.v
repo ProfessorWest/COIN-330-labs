898d934a16558d82ece63201d0e554e2a1715d3d
TheSiahSound <bjosiah23@gmail.com>
2015-02-11 22:15:55 -0500

    Flip-Flop tests

--- lab02/lab02p2test.v
+++ lab02/lab02p2test.v
@@ -1,5 +1,5 @@
 `timescale 100ns/100ps
-module lab02p2test;
+module lab02p2test();
 
 reg clk = 0;
 reg in1 = 0;
@@ -7,15 +7,17 @@ reg in1 = 0;
 wire out;
 wire nout;
 
-always #1 clk = !clk;
+always #1 clk = !clk;	//update clock
 
 lab02p2 testing(in1, clk, out, nout);
 
 initial begin
-	in1 = 1'b0;
+	#1 in1 = 1'b0;
 	$monitor("in1=%b, clock=%b, out=%b, nout=%b", in1, $time, out, nout);
 	$dumpfile("lab02test.vcd");
-	$dumpvars(0,lab02test);
-
+	$dumpvars(0,lab02p2);
+	#2 in1= 1'b1;
+	#2 in1= 1'b0;
+	#12 $finish();	//simulation has finished
 	end
 endmodule
