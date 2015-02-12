871867fc460f4e3ddfdce2ff80bc1574b47abd9e
Tyager44 <teyager@gmail.com>
2015-02-11 23:25:25 -0500

    Updating test module

--- lab02/DFlipFlopTrevorYagerTest.v
+++ lab02/DFlipFlopTrevorYagerTest.v
@@ -1,18 +1,21 @@
-`timescale 100ns/100ps
+`timescale 10ns/1ps
 module DFlipFlopTrevorYagerTest();
 
-wire Q, QNOT;
+wire OUTPUTQ, OUTPUTQNOT;
 reg CLKREG = 0, DREG;
 
 always #1 CLKREG = !CLKREG;
 
-DFlipFlopTrevorYager Lab2FlipFlop(CLKREG, DREG, Q, QNOT);
+DFlipFlopTrevorYager Lab2FlipFlop(CLKREG, DREG, OUTPUTQ, OUTPUTQNOT);
 
 initial begin
-  DREG = 1'b0;
-  $monitor("CLKREG = %b, DREG = %b, Q = %b, QNOT = %b", CLKREG, DREG, Q, QNOT);
+  #1 DREG = 1'b1;
+  $monitor("CLKREG = %b, DREG = %b, OUTPUTQ = %b, OUTPUTQNOT = %b", CLKREG, DREG, OUTPUTQ, OUTPUTQNOT);
   $dumpfile("DFlipFlopTrevorYagerDmpFile.vcd");
   $dumpvars(0, DFlipFlopTrevorYagerTest);
+  #2 DREG = 1'b0;
+  #2 DREG = 1'b1;
+  #4 $finish();	
 
 end
 
