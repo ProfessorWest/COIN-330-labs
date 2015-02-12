4c3b5e08ddee31380d74a0329070234a224441df
Matthew <mwtimmons@csustudent.net>
2015-02-12 12:01:55 -0500

    tried to fix testing

--- lab02/Lab2bMatt-Test.v
+++ lab02/Lab2bMatt-Test.v
@@ -1,18 +1,21 @@
-`timescale 100ns / 100ps
+`timescale 10ns / 1ns
 module lab2bTest();
 
 reg clk = 0;
 always #1 clk = !clk;
-reg [0:0] D;
 wire Q;
 wire QNot;
+reg [0:0] D;
 
-lab2b uut (D,clk,Q,QNot);
+Lab2b uut(D,clk,Q,QNot);
 
 initial begin
 	D = 1'b1;
 	$monitor("At time %t, D = %b, Q = %b, QNot = %b", $time, D, Q, QNot);
 	$dumpfile ("test.vcd");
 	$dumpvars(0,lab2bTest);
+	#4 D = 1'b0;
+	#4 D = 1'b1;
+	#4 $finish();
 	end
 endmodule
