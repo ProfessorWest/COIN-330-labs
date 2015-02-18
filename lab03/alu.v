22464452c6f822c8a53a43da1f0c20277b63e512
Tyler Hall <apple@Tylers-MacBook-Pro.local>
2015-02-18 10:13:34 -0500

    updated alu and adder

--- lab03/alu.v
+++ lab03/alu.v
@@ -1,14 +1,14 @@
 module alu(A, B, op, eq, out);
   input [31:0] A, B;
-  input [2:0] op; //opcode
-  output [0:0] eq; // equality bit
+  input op; //opcode
   output [31:0] out; // output
+  output eq; // equality bit
 
   //assign out = A + B;
   //nand(out, A, B);
   //xnor(eq, A, B);
 
-  assign out = (op==1)?(A + B): // add
-               (op==2)?(A ~& B):1'bx; // nand
-  assign eq  = (op==3)?(A == B):1'bx; // eq
+  assign out = (op==0)?(A + B): // add
+               (op==1)?(A ~& B):1'bx; // nand
+  assign eq  = (A == B); // eq
 endmodule
