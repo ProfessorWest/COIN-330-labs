8e9ff874c4215f0bf1f51159f42ee058ed50ab40
Tyler Hall <apple@Tylers-MacBook-Pro.local>
2015-04-23 16:32:32 -0400

    adjusted 3 bit mux

--- lab04/lc330-single-cycle.v
+++ lab04/lc330-single-cycle.v
@@ -42,8 +42,9 @@ module lc330sc(clk, rst);
     instrmem instr_mem(pc, instr, clk, rst);
     adder32 clockIncrement(pc, one, nextPC);
 
-    mux3bit instr_mux(instr_mux_out, instr[18:16], instr[2:0], rom_out[6]);
-    mux32bit ALU_mem(ALU_mem_out, data_mem_out, ALU_out, rom_out[5]);
+    mux3bit instr_mux(instr_mux_out, instr[2:0], instr[18:16], rom_out[6]);
+  //  mux3bit instr_mux(instr_mux_out, instr[18:16], instr[2:0], rom_out[6]);
+    mux32bit ALU_mem(ALU_mem_out, ALU_out, data_mem_out, rom_out[5]);
 
     signextend ex_instr(instr[15:0], sign_out);
     regfile8x32r2w1 reg_file(instr[18:16], instr[21:19], instr_mux_out, rom_out[4], ALU_mem_out, reg_outA, reg_outB, clk, rst);
@@ -66,7 +67,7 @@ module lc330sc(clk, rst);
             one <= 32'b00000000000000000000000000000001;
 	      end
         else begin
-	          $display("pc = %b, one = %b, nextPC = %b", pc, one, nextPC);
+	          //$display("pc = %b, one = %b, nextPC = %b", pc, one, nextPC);
             pc <= nextPC;
 	      end
     end
